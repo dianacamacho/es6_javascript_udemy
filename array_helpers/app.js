d57c3526e6f710441c8161098ee1a38e16cda2fc
dianacamacho <dcam1901@yahoo.com>
2017-03-31 16:33:55 -0500

    for of loop

--- array_helpers/app.js
+++ array_helpers/app.js
@@ -76,6 +76,27 @@ images.forEach(calcArea);
 
 */
 
+// more array helpers with ES6
+
+/*
+// For Of Loop
+// another way of iterating through array 
+
+const colors = ['red', 'green', 'blue'];
+
+for (let color of colors) {
+  console.log(color);
+}
+
+const numbers = [1,2,3,4,5];
+let total = 0;
+
+for (let number of numbers) {
+  total += number;
+}
+
+console.log(total);
+*/
 
 /*
 // Map
